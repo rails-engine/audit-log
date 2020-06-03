3bb590e3d0f4c11dbe22e0fcc4b2eaf45a236459
Jason Lee <huacnlee@gmail.com>
2020-06-04 01:01:54 +0800

    Fix use bigint column type

--- db/migrate/20190527035005_create_audit_logs.rb
+++ db/migrate/20190527035005_create_audit_logs.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateAuditLogs < ActiveRecord::Migration[5.0]
   def change
     create_table 'audit_logs', force: :cascade do |t|
       t.string 'action', null: false
-      t.integer 'user_id'
-      t.integer 'record_id'
+      t.bigint 'user_id'
+      t.bigint 'record_id'
       t.string 'record_type'
       t.text 'payload'
       t.text 'request'
