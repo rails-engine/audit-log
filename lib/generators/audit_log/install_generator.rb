d3199d5d9f7670fe7bbc4c739361f6ac122edf64
Justin Wong <justincadburywong@gmail.com>
2022-01-06 23:28:07 -0800

    reescape erb - did i bundle a fresh copy?

--- lib/generators/audit_log/install_generator.rb
+++ lib/generators/audit_log/install_generator.rb
@@ -12,9 +12,9 @@ module AuditLog
         template 'config/locales/audit-log.yml', 'config/locales/audit-log.yml'
         template 'app/assets/stylesheets/audit-log/application.css', 'app/assets/stylesheets/audit-log/application.css'
         template 'app/controllers/audit_log/logs_controller.rb', 'app/controllers/audit_log/logs_controller.rb'
-        template 'app/views/audit_log/logs/index.html.erb.tt', 'app/views/audit_log/logs/index.html.erb'
-        template 'app/views/audit_log/logs/show.html.erb.tt', 'app/views/audit_log/logs/show.html.erb'
-        template 'app/views/layouts/audit-log/application.html.erb.tt', 'app/views/layouts/audit-log/application.html.erb'
+        template 'app/views/audit_log/logs/index.html.erb', 'app/views/audit_log/logs/index.html.erb'
+        template 'app/views/audit_log/logs/show.html.erb', 'app/views/audit_log/logs/show.html.erb'
+        template 'app/views/layouts/audit-log/application.html.erb', 'app/views/layouts/audit-log/application.html.erb'
       end
 
       def add_migrations
